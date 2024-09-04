a45ae5ae13c0c9ed3ad335d627fac219113f4840
Sarkor <abilkosimovsarkor@gmail.com>
2024-09-04 20:03:03 +0500

    feat: add something

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import NavBar from "@/components/Navbar"
 import HomeInner from "@/components/HomeInner"
+import OnBoarding from "@/components/OnBoarding"
 
 const Home = () => {
   return <>
     <HomeInner />
     <NavBar />
+    <OnBoarding />
   </>
 }
 
