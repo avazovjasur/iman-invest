95f42416cc646d573913c89783377ae29e56a665
Sarkor <abilkosimovsarkor@gmail.com>
2024-09-09 23:51:11 +0500

    feat: add projects inner page

---
+++ src/components/ProjectsInner/index.jsx
@@ -0,0 +1,129 @@
+import styles from './ProjectsInner.module.scss'
+
+
+const ProjectsInner = () => {
+
+  return <>
+    <div className={styles.content}>
+      <h2 className={styles.contentTitle}>
+        Инвестиции в проекты
+      </h2>
+      <div className={styles.goalHeader}>
+        <ul className={styles.headerList}>
+          <li className={styles.headerListItem}>
+            <div className={styles.headerListItemInner}>
+              <img src="aim-bg.png" alt="aim" />
+              <h3>
+                Рекордные <br /> ставки <br /> по целям
+              </h3>
+            </div>
+          </li>
+          <li className={styles.headerListItem}>
+            <div className={styles.headerListItemInner}>
+              <img src="aim-bg.png" alt="aim" />
+              <h3>
+                Рекордные <br /> ставки <br /> по целям
+              </h3>
+            </div>
+          </li>
+          <li className={styles.headerListItem}>
+            <div className={styles.headerListItemInner}>
+              <img src="aim-bg.png" alt="aim" />
+              <h3>
+                Рекордные <br /> ставки <br /> по целям
+              </h3>
+            </div>
+          </li>
+          <li className={styles.headerListItem}>
+            <div className={styles.headerListItemInner}>
+              <img src="aim-bg.png" alt="aim" />
+              <h3>
+                Рекордные <br /> ставки <br /> по целям
+              </h3>
+            </div>
+          </li>
+          <li className={styles.headerListItem}>
+            <div className={styles.headerListItemInner}>
+              <img src="aim-bg.png" alt="aim" />
+              <h3>
+                Рекордные <br /> ставки <br /> по целям
+              </h3>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.contentWrapper}>
+        <ul className={styles.contentList}>
+          <li className={styles.contentListItem}>
+            <div className={styles.contentImg}>
+              <img src="./project.png" alt="project" />
+              <span>Скоро</span>
+            </div>
+            <div className={styles.contentBox}>
+              <h3 className={styles.contentBoxTitle}>ЖК «Center-1»</h3>
+              <h4 className={styles.contentBotSubtitle}>Инвестиция в постройку</h4>
+              <div className={styles.contentBoxLine}></div>
+              <ul className={styles.contentBoxList}>
+                <li className={styles.contentBoxListItem}>
+                  От 12 млн
+                </li>
+                <li className={styles.contentBoxListItem}>
+                  На 18 мес
+                </li>
+                <li className={`${styles.contentBoxListItem} ${styles.active}`}>
+                  42% в год
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li className={styles.contentListItem}>
+            <div className={styles.contentImg}>
+              <img src="./project.png" alt="project" />
+              <span>Скоро</span>
+            </div>
+            <div className={styles.contentBox}>
+              <h3 className={styles.contentBoxTitle}>ЖК «Center-1»</h3>
+              <h4 className={styles.contentBotSubtitle}>Инвестиция в постройку</h4>
+              <div className={styles.contentBoxLine}></div>
+              <ul className={styles.contentBoxList}>
+                <li className={styles.contentBoxListItem}>
+                  От 12 млн
+                </li>
+                <li className={styles.contentBoxListItem}>
+                  На 18 мес
+                </li>
+                <li className={`${styles.contentBoxListItem} ${styles.active}`}>
+                  42% в год
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li className={styles.contentListItem}>
+            <div className={styles.contentImg}>
+              <img src="./project.png" alt="project" />
+              <span>Скоро</span>
+            </div>
+            <div className={styles.contentBox}>
+              <h3 className={styles.contentBoxTitle}>ЖК «Center-1»</h3>
+              <h4 className={styles.contentBotSubtitle}>Инвестиция в постройку</h4>
+              <div className={styles.contentBoxLine}></div>
+              <ul className={styles.contentBoxList}>
+                <li className={styles.contentBoxListItem}>
+                  От 12 млн
+                </li>
+                <li className={styles.contentBoxListItem}>
+                  На 18 мес
+                </li>
+                <li className={`${styles.contentBoxListItem} ${styles.active}`}>
+                  42% в год
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </>
+}
+
+export default ProjectsInner
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
+
 import NavBar from "@/components/Navbar"
 import HomeInner from "@/components/HomeInner"
+import ProjectsInner from "@/components/ProjectsInner"
 import OnBoarding from "@/components/OnBoarding"
 
 const Home = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return <>
-    <HomeInner />
-    <NavBar />
+    {activeIndex === 0 && <HomeInner />}
+    {activeIndex === 1 && <ProjectsInner />}
+    <NavBar activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
     {/* <OnBoarding /> */}
   </>
 }
