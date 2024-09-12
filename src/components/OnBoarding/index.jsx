ff6494ba384e4c64dc9cc6b143ca74df42e9c559
Sarkor <abilkosimovsarkor@gmail.com>
2024-09-12 22:21:22 +0500

    fix

--- src/components/OnBoarding/index.jsx
+++ src/components/OnBoarding/index.jsx
@@ -1,90 +1,87 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './OnBoarding.module.scss';
 
-const OnBoarding = () => {
+const OnBoarding = ({ onClose }) => {
   const [step, setStep] = useState(1);
-  const [isShown, setIsShown] = useState(false);
-
-  useEffect(() => {
-    const hasSeenOnBoarding = localStorage.getItem('hasSeenOnBoarding');
-    if (!hasSeenOnBoarding) {
-      setIsShown(true);
-    }
-  }, []);
 
   const handleNextStep = () => {
     setStep(step + 1);
   };
 
   const handleSkip = () => {
-    localStorage.setItem('hasSeenOnBoarding', 'true');
-    setIsShown(false);
+    setStep(0);
+    onClose();  // Закрываем OnBoarding
   };
 
-  if (!isShown) {
-    return null;
-  }
-
   return (
-    <div className={styles.modal}>
-      <div className={styles.modalWrapper}>
-        <div className={styles.modalClose}>
-          <button className={`${styles.modalCloseBtn} ${step === 3 ? styles.active : ''}`} onClick={handleSkip}>Пропустить</button>
-        </div>
-        {step === 1 &&
-          <div className={styles.modalStepOne}>
-            <div className={styles.modalImage}>
-              <img src="step1.png" alt="image" />
+    <>
+      {step !== 0 && (
+        <div className={styles.modal}>
+          <div className={styles.modalWrapper}>
+            <div className={styles.modalClose}>
+              <button
+                className={`${styles.modalCloseBtn} ${step === 3 ? styles.active : ''}`}
+                onClick={handleSkip}
+              >
+                Пропустить
+              </button>
             </div>
-            <div className={styles.stepOneContent}>
-              <h2 className={styles.stepTitle}>
-                Доходность до <span>~30%</span> годовых
-              </h2>
-              <p className={styles.stepText}>Достигать цели легко. Мы поможем</p>
+            {step === 1 && (
+              <div className={styles.modalStepOne}>
+                <div className={styles.modalImage}>
+                  <img src="step1.png" alt="image" />
+                </div>
+                <div className={styles.stepOneContent}>
+                  <h2 className={styles.stepTitle}>
+                    Доходность до <span>~30%</span> годовых
+                  </h2>
+                  <p className={styles.stepText}>Достигать цели легко. Мы поможем</p>
+                </div>
+              </div>
+            )}
+            {step === 2 && (
+              <div className={styles.modalStepTwo}>
+                <div className={styles.modalImage}>
+                  <img src="step2.png" alt="image" />
+                </div>
+                <div className={styles.stepTwoContent}>
+                  <h2 className={styles.stepTitle}>
+                    <div className={styles.line}> Ростовщичество.</div>
+                    <br /> Партнерство!
+                  </h2>
+                  <p className={styles.stepText}>Вы копите - мы заботимся о принципах</p>
+                </div>
+              </div>
+            )}
+            {step === 3 && (
+              <div className={styles.modalStepThree}>
+                <div className={styles.modalImage}>
+                  <img src="step3.png" alt="image" />
+                </div>
+                <div className={styles.stepTwoContent}>
+                  <h2 className={styles.stepTitle}>Начните уже сейчас</h2>
+                  <p className={styles.stepText}>
+                    Чтобы начать копить, нужно только выбрать цель и срок
+                  </p>
+                </div>
+              </div>
+            )}
+            <div className={styles.modalPagination}>
+              <ul className={styles.modalPaginationList}>
+                <li className={`${styles.modalPaginationItem} ${step === 1 ? styles.active : ''}`}></li>
+                <li className={`${styles.modalPaginationItem} ${step === 2 ? styles.active : ''}`}></li>
+                <li className={`${styles.modalPaginationItem} ${step === 3 ? styles.active : ''}`}></li>
+              </ul>
+            </div>
+            <div className={styles.modalButton}>
+              {step !== 3 && <button onClick={handleNextStep}>Далее</button>}
+              {step === 3 && <button onClick={handleSkip}>Начать</button>}
             </div>
           </div>
-        }
-        {step === 2 &&
-          <div className={styles.modalStepTwo}>
-            <div className={styles.modalImage}>
-              <img src="step2.png" alt="image" />
-            </div>
-            <div className={styles.stepTwoContent}>
-              <h2 className={styles.stepTitle}>
-                <div className={styles.line}> Ростовщичество.</div>
-                <br />  Партнерство!
-              </h2>
-              <p className={styles.stepText}>Вы копите - мы заботимся о принципах</p>
-            </div>
-          </div>
-        }
-        {step === 3 &&
-          <div className={styles.modalStepThree}>
-            <div className={styles.modalImage}>
-              <img src="step3.png" alt="image" />
-            </div>
-            <div className={styles.stepTwoContent}>
-              <h2 className={styles.stepTitle}>
-                Начните уже сейчас
-              </h2>
-              <p className={styles.stepText}>Чтобы начать копить, нужно только выбрать цель и срок</p>
-            </div>
-          </div>
-        }
-        <div className={styles.modalPagination}>
-          <ul className={styles.modalPaginationList}>
-            <li className={`${styles.modalPaginationItem} ${step === 1 ? styles.active : ''}`}></li>
-            <li className={`${styles.modalPaginationItem} ${step === 2 ? styles.active : ''}`}></li>
-            <li className={`${styles.modalPaginationItem} ${step === 3 ? styles.active : ''}`}></li>
-          </ul>
         </div>
-        <div className={styles.modalButton}>
-          {step !== 3 && <button onClick={handleNextStep}>Далее</button>}
-          {step === 3 && <button onClick={handleSkip}>Начать</button>}
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
-}
+};
 
 export default OnBoarding;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-import NavBar from "@/components/Navbar"
-import HomeInner from "@/components/HomeInner"
-import ProjectsInner from "@/components/ProjectsInner"
-import OnBoarding from "@/components/OnBoarding"
+import NavBar from "@/components/Navbar";
+import HomeInner from "@/components/HomeInner";
+import ProjectsInner from "@/components/ProjectsInner";
+import OnBoarding from "@/components/OnBoarding";
 
 const Home = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [isOnBoardingVisible, setIsOnBoardingVisible] = useState(true);
 
-  return <>
-    {activeIndex === 0 && <HomeInner />}
-    {activeIndex === 1 && <ProjectsInner />}
-    <NavBar activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
-    <OnBoarding />
-  </>
-}
+  useEffect(() => {
+    if (isOnBoardingVisible) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
 
-export default Home
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isOnBoardingVisible]);
+
+  const handleOnBoardingClose = () => {
+    setIsOnBoardingVisible(false);
+  };
+
+  return (
+    <>
+      {activeIndex === 0 && <HomeInner />}
+      {activeIndex === 1 && <ProjectsInner />}
+      <NavBar activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
+      {isOnBoardingVisible && <OnBoarding onClose={handleOnBoardingClose} />}
+    </>
+  );
+};
+
+export default Home;
