62b972fbbdbcbc9587d6724f4d2856d0fa331f05
JasurAvazov <jasuravazov4@gmail.com>
2024-09-12 13:20:10 +0500

    here

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,26 +9,11 @@ export default function Home() {
     router.push('/registration/lang')
   }
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     redirectToReg()
-  //   }, 3000)
-  // }, [])
-
   useEffect(() => {
-    if (typeof window !== 'undefined' && window.Telegram) {
-      const tg = window.Telegram.WebApp;
-
-      // Настраиваем приложение для работы внутри Telegram
-      tg.expand();  // Разворачиваем приложение на полный экран
-
-      tg.MainButton.text = "Продолжить";  // Устанавливаем текст для основной кнопки
-      tg.MainButton.show();  // Показываем основную кнопку
-      tg.MainButton.onClick(() => {
-        alert("Кнопка нажата!");
-      });
-    }
-  }, []);
+    setTimeout(() => {
+      redirectToReg()
+    }, 3000)
+  }, [])
 
   return (
     <>
